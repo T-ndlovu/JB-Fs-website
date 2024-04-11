e337867f8fd26458c23168341e58318976d22a09
Lesedi <molemilesedi@gmail.com>
2024-04-11 22:10:14 +0200

    - Updated category_template.php to use CSS elements in the extra.css file

--- category_template.php
+++ category_template.php
@@ -14,10 +14,10 @@ if (isset($_GET['categoryid'])) {
         $productsStmt = $pdo->prepare("SELECT * FROM product WHERE CategoryID = ?");
         $productsStmt->execute([$categoryId]);
         $products = $productsStmt->fetchAll(PDO::FETCH_ASSOC);
-        ?>
+?>
+        <link rel="stylesheet" href="./public/extra.css">
         <section style="padding-top:10px; padding-bottom:20px">
-            <div
-                style="background-image: url('https://shorturl.at/qsNP0'); background-size: cover; background-position: center; text-align: center; padding: 50px 0;">
+            <div style="background-image: url('https://shorturl.at/qsNP0'); background-size: cover; background-position: center; text-align: center; padding: 50px 0;">
                 <h1 style="font-size: 36px; margin: 10px;margin-bottom:10px; color:white; font-weight:bold">
                     <?php echo $category['Category_Name']; ?>
                 </h1>
@@ -25,7 +25,7 @@ if (isset($_GET['categoryid'])) {
         </section>
 
         <div class="listed-products">
-            <?php foreach ($products as $product): ?>
+            <?php foreach ($products as $product) : ?>
                 <?php
                 $sql_image = "SELECT * FROM productimage WHERE ProductID = :product_id LIMIT 1";
                 $stmt_image = $pdo->prepare($sql_image);
@@ -46,7 +46,7 @@ if (isset($_GET['categoryid'])) {
 
             <?php endforeach; ?>
         </div>
-        <?php
+<?php
     } else {
         echo "Category not found!";
     }
@@ -55,69 +55,5 @@ if (isset($_GET['categoryid'])) {
 }
 
 ?>
-<style>
-    .individual-product {
-        margin: 0 auto;
-        margin-bottom: 20px;
-        width: 400px;
-        aspect-ratio: 5/4;
-    }
-
-    .individual-product img {
-        border-radius: 8px;
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        max-height: 450px;
-        max-width: 600px;
-    }
-
-    .individual-product img {
-        margin: 0 auto;
-    }
-
-    .products {
-        padding: 2%;
-        max-width: 1236px;
-        margin: 0 auto;
-    }
-
-    .products h1 {
-        padding: 2%;
-    }
-
-    .products p,
-    .product-details {
-        text-align: center;
-    }
-
-    .product-name {
-        font-size: large;
-    }
-
-    .product-price {
-        font-size: large;
-        font-weight: bold;
-    }
-
-    @media (min-width: 1068px) {
-        .individual-product img {
-            width: 100%;
-            height: 100%;
-        }
-
-        .individual-product {
-            width: 400px;
-            aspect-ratio: 5/4;
-        }
-
-        .listed-products {
-            display: grid;
-            grid-template-rows: 1fr 1fr;
-            grid-template-columns: 1fr 1fr 1fr;
-            gap: 30px;
-        }
-    }
-</style>
 
 <?= template_footer() ?>
