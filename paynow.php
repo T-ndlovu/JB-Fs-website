989cb99b273ab3f53da74a1a4d018167f5e7dcba
Lesedi <molemilesedi@gmail.com>
2024-04-20 11:37:50 +0200

    added generateSignature functions

--- paynow.php
+++ paynow.php
@@ -1,3 +1,31 @@
+<?php
+
+/**
+ * @param array $data
+ * @param null $passPhrase
+ * @return string
+ */
+
+//*  The signature will create a hash that all the form values are submitting
+function generateSignature($data, $passPhrase = null)
+{
+    // Create parameter string
+    $pfOutput = '';
+    foreach ($data as $key => $val) {
+        if ($val !== '') {
+            $pfOutput .= $key . '=' . urlencode(trim($val)) . '&';
+        }
+    }
+    // Remove last ampersand
+    $getString = substr($pfOutput, 0, -1);
+    if ($passPhrase !== null) {
+        $getString .= '&passphrase=' . urlencode(trim($passPhrase));
+    }
+    return md5($getString);
+}
+?>
+
+
 <form action="https://www.payfast.co.za/eng/process" method="post">
     <!-- merchant -->
     <input type="hidden" name="merchant_id" value="10000100">
